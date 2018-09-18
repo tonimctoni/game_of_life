7155c5883505d6ff3285a8c8b1fd6de0fc453f4b
tonimctoni <nicoflam@outlook.com>
2018-09-18 10:32:52 +0200

    updated to elm 0.19

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,10 @@
+module Main exposing (main)
+
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Time exposing (Time, second)
+import Time
 import Set
 
 board_size: Int
@@ -41,35 +44,34 @@ r_pentomino_live_cells=
     , (c+8,c-4)
     ]
 
-main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
 
+main : Program () Model Msg
+main =
+  Browser.document
+    { init = init
+    , subscriptions = subscriptions
+    , update = update
+    , view = view
+    }
 
 
 -- MODEL
 
+type alias Model =
+  { live_cells: Set.Set (Int,Int)
+  , running: Bool
+  }
 
-type alias Model = {
-  live_cells: Set.Set (Int,Int),
-  running: Bool
-}
 
-
-init : (Model, Cmd Msg)
-init =
-  (Model Set.empty False, Cmd.none)
+init : () -> (Model, Cmd Msg)
+init _ =
+  ({live_cells=Set.empty, running=False}, Cmd.none)
 
 
 -- UPDATE
 
-
 type Msg
-  = Tick Time
+  = Tick Time.Posix
   | ChangeState (Int,Int)
   | DoOneStep
   | RunStop
@@ -79,7 +81,7 @@ type Msg
   | SetEmpty
 
 game_of_life_iteration: Set.Set (Int,Int) -> Set.Set (Int,Int)
-game_of_life_iteration live_cells =
+game_of_life_iteration live_cells_no_shadow =
   let
     board_poss =
       List.concat (
@@ -117,10 +119,9 @@ game_of_life_iteration live_cells =
         case Set.member pos live_cells of
           True -> if nc<2 then False else if nc<4 then True else False
           False -> if nc==3 then True else False
-      
   in 
     Set.fromList
-    <| List.filter (\pos -> get_live_state live_cells pos) board_poss
+    <| List.filter (\pos -> get_live_state live_cells_no_shadow pos) board_poss
 
 kill_if_alive_and_resurrect_if_dead: Set.Set (Int,Int) -> (Int,Int) -> Set.Set (Int,Int)
 kill_if_alive_and_resurrect_if_dead live_cells pos =
@@ -162,59 +163,62 @@ update msg model =
       then (model, Cmd.none)
       else ({model | live_cells=Set.empty}, Cmd.none)
 
-
 -- SUBSCRIPTIONS
-
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
   if model.running
-  then Time.every (second/5.0) Tick
+  then Time.every 200 Tick
   else Sub.none
-
 
 -- VIEW
 
-
 draw_square : Model -> (Int,Int) -> Html Msg
 draw_square model pos=
-  let
-    squre_style=style [
-      ("padding", "0pt"),
-      ("margin", "1pt"),
-      ("width", "10pt"),
-      ("height", "10pt"),
-      ("float", "left"),
-      ("border-radius", "2px"),
-      case (Set.member pos model.live_cells) of
-        False -> ("background", "#a4dae5")
-        True -> ("background", "black")
-    ]
-  in
-    div [squre_style, onClick (ChangeState pos)] []
+  div
+  [ style "padding" "0pt"
+  , style "margin" "1pt"
+  , style "width" "10pt"
+  , style "height" "10pt"
+  , style "float" "left"
+  , style "border-radius" "2px"
+  , case (Set.member pos model.live_cells) of
+      False -> style "background" "#a4dae5"
+      True -> style "background" "black"
+  , onClick (ChangeState pos)
+  ]
+  []
 
 draw_square_row: Model -> Int -> Html Msg
 draw_square_row model y=
-  div [style [("float", "top")]]
+  div [style "float" "top"]
   (List.map (\x -> draw_square model (x,y)) (List.range 0 (board_size-1)))
 
 draw_board: Model -> Html Msg
 draw_board model=
-  div [style [("background", "#b4eaf5"), ("border-radius", "4px"), ("width", (toString (12*board_size))++"pt"), ("height", (toString (12*board_size))++"pt"), ("padding", "2pt")]]
-  (List.map (\y -> draw_square_row model y) (List.range 0 (board_size-1)))
+  div
+    [ style "background" "#b4eaf5"
+    , style "border-radius" "4px"
+    , style "width" ((String.fromInt (12*board_size))++"pt")
+    , style "height" ((String.fromInt (12*board_size))++"pt")
+    , style "padding" "2pt"
+    ]
+    (List.map (\y -> draw_square_row model y) (List.range 0 (board_size-1)))
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-  div [style [("background", "#B0B0B0"), ("width", "100%"), ("height", "100%"), ("position", "fixed")]]
-  [ div [style [("position", "fixed"), ("top", "50%"), ("left", "50%"), ("transform", "translate(-50%, -50%)")]]
-    [ draw_board model
-    , button [onClick RunStop] [text <| if model.running then "Stop" else "Start"]
-    , button [onClick DoOneStep, disabled model.running] [text "Step"]
-    , div [style [("margin-left", "2cm"), ("float", "right"), ("width", "5cm")]]
-      [ button [style [("float", "top"), ("width", "5cm")], onClick SetPulsarGenerator, disabled model.running] [text "Set Pulsar Generator"]
-      , button [style [("float", "top"), ("width", "5cm")], onClick SetPentadecathlon, disabled model.running] [text "Set Pentadecathlon"]
-      , button [style [("float", "top"), ("width", "5cm")], onClick SetR_Pentomino, disabled model.running] [text "Set R-Pentomino"]
-      , button [style [("float", "top"), ("width", "5cm")], onClick SetEmpty, disabled model.running] [text "Set Empty"]
+  Browser.Document "Elm Game of Life"
+  [ div [style "background" "#B0B0B0", style "top" "0", style "left" "0", style "width" "100%", style "height" "100%", style "position" "absolute"]
+    [ div [style "position" "fixed", style "top" "50%", style "left" "50%", style "transform" "translate(-50%, -50%)"]
+      [ draw_board model
+      , button [onClick RunStop] [text <| if model.running then "Stop" else "Start"]
+      , button [onClick DoOneStep, disabled model.running] [text "Step"]
+      , div [style "margin-left" "2cm", style "float" "right", style "width" "5cm"]
+        [ button [style "float" "top", style "width" "5cm", onClick SetPulsarGenerator, disabled model.running] [text "Set Pulsar Generator"]
+        , button [style "float" "top", style "width" "5cm", onClick SetPentadecathlon, disabled model.running] [text "Set Pentadecathlon"]
+        , button [style "float" "top", style "width" "5cm", onClick SetR_Pentomino, disabled model.running] [text "Set R-Pentomino"]
+        , button [style "float" "top", style "width" "5cm", onClick SetEmpty, disabled model.running] [text "Set Empty"]
+        ]
       ]
     ]
   ]
